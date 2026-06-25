bd64fc9063f3e1793981d5e42bdc5dd8bd63de12
Everton554 <darkshouds@gmail.com>
2026-06-25 08:26:00 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <main>
+        
+        <h1>Meu primeiro react</h1>
+
+
+        <h6>Everton Matheus de Borba</h6>
+        <p>Desenvolvimento de Sistemas</p>
+        <p>Guaramirim - Santa Catarina</p>
+        <br></br>
+        <p>Estudo na Escola <strong>CEDUP Perfeito Manoel de Aguiar<strong></strong></strong></p>
+        
+        
+        <div>
+        <br></br>
+        <p>Meus Objetivos: 
+          <br></br>Alcançar o sucesso na área de tecnologia
+          <br></br>Me especializar futuramente na area e conseguir um bom emprego 
+          <br></br>Ter com quem compartilhar conhecimento.</p>
+      <br></br>
+      </div>
+
+      <p><p>Com o curso eu desejaria 
+          <br></br>Ter meu proprio negócio na área de tecnologia
+          <br></br>Poder ajudar as pessoas com meu conhecimento.</p></p>
+      </main>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
